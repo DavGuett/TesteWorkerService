a3e7a96615cec0627df99f1e213ca08164ec674c
DavGuett <soulsec@protonmail.com>
2023-05-16 20:26:16 -0300

    Adicionado logs

--- Worker.cs
+++ Worker.cs
@@ -3,16 +3,32 @@ namespace TesteWorkerService
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private HttpClient Client;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
         }
 
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            Client = new HttpClient();
+            return base.StartAsync(cancellationToken);
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
+                var result = await Client.GetAsync("https://www.proa.tec.br");
+                if(result.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation($"Website is up and running. Status code {result.StatusCode}");
+                }
+                else
+                {
+                    _logger.LogError($"Website is down. Status code: {result.StatusCode}");
+                }
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
